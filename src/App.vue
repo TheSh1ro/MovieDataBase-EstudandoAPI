1f90d7048ed06d3191b484150a9c817fc18d6456
PauloK3tchup <passandoalimpo13@gmail.com>
2022-09-12 15:16:03 -0300

    Criada a pagina home + css

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,44 @@ import PageHeader from "./components/PageHeader.vue";
 </script>
 
 <template>
-  <PageHeader></PageHeader>
-  <RouterView />
+  <header id="header">
+    <PageHeader></PageHeader>
+  </header>
+  <main id="main">
+    <RouterView></RouterView>
+  </main>
 </template>
 
-<style scoped></style>
+<style>
+body {
+  font-size: 16px;
+}
+* {
+  margin: 0px;
+  padding: 0px;
+  list-style-type: none;
+  text-decoration: none;
+  color: inherit;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 10vh 1fr;
+}
+
+#main {
+  min-height: 90vh;
+
+  color: black;
+  background-color: #17a7a8;
+  font-size: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  padding: 5vh 5vw;
+}
+</style>
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,7 +1,35 @@
-<script setup></script>
-
 <template>
-  xd
+  <div class="pageview-container">
+    <span class="pageview-button">Gênero</span>
+    <span class="pageview-button">Em alta</span>
+    <span class="pageview-button">Atores</span>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#header {
+  color: #fe958f;
+  background-color: #122f51;
+  font-size: 1.5rem;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.pageview-container {
+  cursor: default;
+
+  display: flex;
+  flex-direction: row;
+  gap: 1vw;
+}
+.pageview-button {
+  padding: 5px;
+  cursor: pointer;
+}
+.pageview-button:hover {
+  background-color: rgb(151, 31, 31);
+  color: #ffb2ae;
+}
+</style>
